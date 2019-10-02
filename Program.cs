40a65dc12d31faeef03102909084fd1c352c4dd6
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2019-10-02 09:27:38 -0700

    feat: testing for C# testing input and output in form of quiz

--- Program.cs
+++ Program.cs
@@ -6,62 +6,65 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-            double operand1, operand2;
-            char op;
-            Console.Write("Enter First Number to Calculate: ");
-            operand1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter Second Number to Calculate: ");
-            operand2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter An Operator:");
-            op = Convert.ToChar(Console.ReadLine());
-            if (op == '+')
-                Console.WriteLine(operand1 + operand2);
-            else if (op == '-')
-                Console.WriteLine(operand1 - operand2);
-            else if (op == '*')
-                Console.WriteLine(operand1 * operand2);
-            else if (op == '/')
-                Console.WriteLine(operand1 / operand2);
-            else
-                Console.WriteLine("Try again foolio");
-            Console.ReadKey();
 
-            // // string name;
-            // String answer = "Watson";
-            // String response = "";
-            // Console.Write("Let's play a guessing game...");
-            // Console.WriteLine("You get three tries...");
-            // Console.WriteLine("What is the name of the computer on Jeopardy?");
-            // response = Console.ReadLine();
-            // if (response == answer)
-            // {
-            //     Console.WriteLine("That is correct!");
-            // }
+            // Calculator
+            // double operand1, operand2;
+            // char op;
+            // Console.Write("Enter First Number to Calculate: ");
+            // operand1 = Convert.ToDouble(Console.ReadLine());
+            // Console.Write("Enter Second Number to Calculate: ");
+            // operand2 = Convert.ToDouble(Console.ReadLine());
+            // Console.Write("Enter An Operator:");
+            // op = Convert.ToChar(Console.ReadLine());
+            // if (op == '+')
+            //     Console.WriteLine(operand1 + operand2);
+            // else if (op == '-')
+            //     Console.WriteLine(operand1 - operand2);
+            // else if (op == '*')
+            //     Console.WriteLine(operand1 * operand2);
+            // else if (op == '/')
+            //     Console.WriteLine(operand1 / operand2);
             // else
-            // {
-            //     Console.WriteLine("Sorry try again....");
-            //     response = Console.ReadLine();
-            //     if (response == answer)
-            //     {
-            //         Console.WriteLine("You rule!!!");
-            //     }
-            //     else
-            //     {
-            //         Console.WriteLine("Sorry try again dorkus....");
-            //         response = Console.ReadLine();
-            //         if (response == answer)
-            //         {
-            //             Console.WriteLine("you are the best go to DragonForce concert");
-            //         }
-            //         else
-            //         {
-            //             Console.WriteLine("Sorry you cannot go back to being a winner.  You are forever a loser at the game of life....");
-            //         }
+            //     Console.WriteLine("Try again foolio");
+            // Console.ReadKey();
 
-            //     }
+            //quiz to test con
+            // string name;
+            String answer = "Watson";
+            String response = "";
+            Console.Write("Let's play a guessing game...");
+            Console.WriteLine("You get three tries...");
+            Console.WriteLine("What is the name of the computer on Jeopardy?");
+            response = Console.ReadLine();
+            if (response == answer)
+            {
+                Console.WriteLine("That is correct!");
+            }
+            else
+            {
+                Console.WriteLine("Sorry try again....");
+                response = Console.ReadLine();
+                if (response == answer)
+                {
+                    Console.WriteLine("You rule!!!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry try again dorkus....");
+                    response = Console.ReadLine();
+                    if (response == answer)
+                    {
+                        Console.WriteLine("you are the best go to DragonForce concert");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry you cannot go back to being a winner.  You are forever a loser at the game of life....");
+                    }
+
+                }
 
 
-            // }
+            }
 
             // int x = 15;
             // // int y = 6;
