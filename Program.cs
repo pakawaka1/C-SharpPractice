4a69b9175bd54e4b4919da2c156e1c246f7c1a77
David L Cervantes <davidlcervantes@Davids-MacBook-Pro.local>
2019-10-02 10:48:31 -0700

    feat: using while loop for quiz

--- Program.cs
+++ Program.cs
@@ -6,15 +6,13 @@ namespace myApp
     {
         static void Main(string[] args)
         {
-
-            // Calculator
             // double operand1, operand2;
             // char op;
             // Console.Write("Enter First Number to Calculate: ");
             // operand1 = Convert.ToDouble(Console.ReadLine());
             // Console.Write("Enter Second Number to Calculate: ");
             // operand2 = Convert.ToDouble(Console.ReadLine());
-            // Console.Write("Enter An Operator:");
+            // Console.Write("Enter An Operator");
             // op = Convert.ToChar(Console.ReadLine());
             // if (op == '+')
             //     Console.WriteLine(operand1 + operand2);
@@ -28,55 +26,70 @@ namespace myApp
             //     Console.WriteLine("Try again foolio");
             // Console.ReadKey();
 
-            //quiz to test con
-            // string name;
+            // // string name;
+            // String answer = "Watson";
+            // String response = "";
+            // Console.Write("Let's play a guessing game...");
+            // Console.WriteLine("You get three tries...");
+            // Console.WriteLine("What is the name of the computer on Jeopardy?");
+            // response = Console.ReadLine();
+            // if (response == answer)
+            // {
+            //     Console.WriteLine("That is correct!");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Sorry try again....");
+            //     response = Console.ReadLine();
+            //     if (response == answer)
+            //     {
+            //         Console.WriteLine("You rule!!!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("Sorry try again dorkus....");
+            //         response = Console.ReadLine();
+            //         if (response == answer)
+            //         {
+            //             Console.WriteLine("you are the best go to DragonForce concert");
+            //         }
+            //         else
+            //         {
+            //             Console.WriteLine("Sorry you cannot go back to being a winner.  You are forever a loser at the game of life....");
+            //         }
+
+            //     }
+
+
+            // }
+
+            // while loop
             String answer = "Watson";
             String response = "";
-            Console.Write("Let's play a guessing game...");
+            Console.WriteLine("Let's play a guessing game...");
             Console.WriteLine("You get three tries...");
-            Console.WriteLine("What is the name of the computer on Jeopardy?");
-            response = Console.ReadLine();
-            if (response == answer)
+            int tries = 1;
+            while (tries <= 3)
             {
-                Console.WriteLine("That is correct!");
-            }
-            else
-            {
-                Console.WriteLine("Sorry try again....");
+                Console.WriteLine("What is the name of the computer on Jeopardy?");
                 response = Console.ReadLine();
                 if (response == answer)
                 {
-                    Console.WriteLine("You rule!!!");
+                    Console.WriteLine("That is correct!");
+                    break;
                 }
-                else
-                {
-                    Console.WriteLine("Sorry try again dorkus....");
-                    response = Console.ReadLine();
-                    if (response == answer)
-                    {
-                        Console.WriteLine("you are the best go to DragonForce concert");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Sorry you cannot go back to being a winner.  You are forever a loser at the game of life....");
-                    }
-
-                }
-
-
+                else if (tries < 3)
+                    Console.WriteLine("Sorry try again....");
+                ++tries;
+            }
+            if (response != answer)
+            {
+                Console.WriteLine("Sorry you cannot go back to being a winner.  You are forever a loser at the game of life....and the answer is Watson");
+                Console.ReadKey();
             }
 
-            // int x = 15;
-            // // int y = 6;
-            // // x %= y;
-
-            // Console.WriteLine("Enter your name: ");
-
-            // name = Console.ReadLine();
-            // Console.WriteLine(name + x);
-
-            // // Console.Write(y);
 
         }
+
     }
 }
